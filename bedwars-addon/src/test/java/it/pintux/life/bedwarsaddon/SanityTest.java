427b93ba33a8351ed28e3ea1691931126e409f58
pintux <pintux98@gmail.com>
2026-06-01 22:36:38 +0300

    test(bedwars-addon): add junit sanity test

---
+++ bedwars-addon/src/test/java/it/pintux/life/bedwarsaddon/SanityTest.java
@@ -0,0 +1,11 @@
+package it.pintux.life.bedwarsaddon;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SanityTest {
+    @Test
+    void junitIsWired() {
+        assertEquals(2, 1 + 1);
+    }
+}
